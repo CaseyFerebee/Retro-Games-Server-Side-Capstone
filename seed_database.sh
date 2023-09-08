5ff9811837f7b3c4a96faa74834c7182fe2af943
Casey Ferebee <cwferebee@gamil.com>
2023-09-08 10:32:50 -0500

    added controllers to controller fixtures

--- seed_database.sh
+++ seed_database.sh
@@ -5,10 +5,10 @@ rm -rf ./retroapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations retroapi
 python3 manage.py migrate retroapi
-python3 manage.py loaddata condition
+python3 manage.py loaddata conditions
 python3 manage.py loaddata tokens
-python3 manage.py loaddata console
-python3 manage.py loaddata controller
-python3 manage.py loaddata genre
-python3 manage.py loaddata owner
+python3 manage.py loaddata consoles
+python3 manage.py loaddata controllers
+python3 manage.py loaddata genres
+python3 manage.py loaddata owners
 python3 manage.py loaddata users
