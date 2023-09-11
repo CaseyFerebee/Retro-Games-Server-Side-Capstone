2b5c31d1f53fdca3bc3de27e7a77434effd9bacf
Casey Ferebee <cwferebee@gamil.com>
2023-09-11 14:52:26 -0500

    added create method to gameview

--- retroapi/views/game_view.py
+++ retroapi/views/game_view.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from retroapi.models import Game, Genre, Condition
+from retroapi.models import Game, Genre, Condition, Owner
 
 
 class GameView(ViewSet):
@@ -12,16 +12,37 @@ class GameView(ViewSet):
         game = Game.objects.get(pk=pk)
         serializer = GameSerializer(game)
         return Response(serializer.data)
-    
+
     def list(self, request):
 
         game = Game.objects.all()
         serializer = GameSerializer(game, many=True)
         return Response(serializer.data)
-    
-    
-    
-    
+
+    def create(self, request):
+        print(request.data)
+        owner = Owner.objects.get(user=request.auth.user)
+        condition = Condition.objects.get(pk=request.data["condition"])
+        genre = Genre.objects.get(pk=request.data["genre"])
+
+
+        game = Game.objects.create(
+            owner = owner,
+            description = request.data["description "],
+            releaseDate = request.data["releaseDate "],
+            publisher  = request.data["publisher "],
+            developer = request.data["developer "],
+            modes = request.data["modes "],
+            img  = request.data["img  "],
+            condition=condition,
+            genre=genre
+        )
+        
+        serializer = GameSerializer(post)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
     def destroy(self, request, pk):
         game = Game.objects.get(pk=pk)
         game.delete()
